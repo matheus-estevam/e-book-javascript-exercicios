a6ef7ad6c7176a7883364508c03c7e09a833d880
matheus-estevam <matheusestevam17055@gmail.com>
2024-02-15 20:55:59 -0300

    exercios feitos do 24 ate o 30

---
+++ cap_2_fundamentos_de_javascript/ex27.js
@@ -0,0 +1,3 @@
+for (let index = 1; index <= 10; index++) {
+  console.log(`5 x ${index} = ${5 * index}`);
+}
---
+++ cap_2_fundamentos_de_javascript/ex28.js
@@ -0,0 +1,8 @@
+let i = 1;
+let soma = 0;
+while (i <= 100) {
+  soma += i;
+  i++;
+}
+
+console.log("Soma Total de 1 a 100: ", soma);
